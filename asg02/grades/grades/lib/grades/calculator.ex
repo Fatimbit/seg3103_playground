b3ba0df66b8df331858019acc916a010d416ab0e
Fatimah Vakily <fvaki076@uottawa.ca>
2021-06-10 20:45:17 -0400

    Question 2.1 Refactored Complete

--- asg02/grades/grades/lib/grades/calculator.ex
+++ asg02/grades/grades/lib/grades/calculator.ex
@@ -1,38 +1,33 @@
 defmodule Grades.Calculator do
-  def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+
+
+  def avg(homework, labs) do            #refactored for Question 2.1 (Github commit "Question 2.1 Refactored Complete")
     avg_homework =
       if Enum.count(homework) == 0 do
         0
       else
         Enum.sum(homework) / Enum.count(homework)
-      end
+    end
 
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+  avg_labs =
+    if Enum.count(labs) == 0 do
+      0
+    else
+      Enum.sum(labs) / Enum.count(labs)
+    end
 
+    {avg_homework,avg_labs}
+    end
+
+
+  def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+    {avg_homework, avg_labs} = avg(homework, labs)
     mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
     round(mark * 100)
   end
 
   def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
-
+   {avg_homework, avg_labs} = avg(homework, labs)
     avg_exams = (midterm + final) / 2
 
     num_labs =
@@ -62,19 +57,7 @@ defmodule Grades.Calculator do
   end
 
   def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+  {avg_homework, avg_labs} = avg(homework, labs)
 
     avg_exams = (midterm + final) / 2
 
